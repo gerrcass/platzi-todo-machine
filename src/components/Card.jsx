b2816b4f5e72abff335e403ffb9452ce56945f75
Gerardo Castillo <gerrcass@gmail.com>
2022-12-29 11:01:35 -0400

    TodoForm components completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@
 const Card = ({ children }) => {
     return (
         <div className='h-screen flex flex-col justify-center items-center bg-gradient-to-bl from-gray-200 via-gray-300 to-gray-100'>
-            <div className="w-[25rem] h-[40rem] px-5 py-12 rounded-2xl shadow-xl bg-gradient-to-r from-rose-400 to-orange-300 text-center">
+            <div className="overflow-y-auto  w-[25rem] h-[30rem] px-5 py-12 rounded-2xl shadow-xl bg-gradient-to-r from-rose-400 to-orange-300 text-center">
                 {children}
             </div>
         </div>
